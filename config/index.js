4ababd38e9cc4c3061135d74c75607f2ffa5aded
BlueSeaHeart <36020124+BlueSeaHeart@users.noreply.github.com>
2024-02-21 14:11:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -55,6 +55,14 @@ export const config = {
       city: "西安",
       openUrl: "https://weibo.com"
     },
+    // {
+    //   name: "林",
+    //   id: "oSIm45qKVGQ8dh07K6Y_6vAzIGqc",
+    //   useTemplateId: "1XlaXWwOihXhnvopOWFGSPwjEfuGIUyxnQ4y3zCojnM",
+    //   province: "陕西",
+    //   city: "西安",
+    //   openUrl: "https://weibo.com"
+    // },
 //     {
 //       name: "老婆2",
 //       id: "",
